d137bb3ed8900b0fb4fcbe1c77dff66f4dbf4509
Al Stone <astone@heckgrammar.co.uk>
2024-09-27 09:58:08 +0100

    Added Data types

--- DataTypesY924/Program.cs
+++ DataTypesY924/Program.cs
@@ -4,8 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mr Stone");
-            Console.WriteLine("C# is ace!");
+            // datatype identifier (optional assignment)
+            int myInteger;
+            myInteger = 42;
+            Console.WriteLine(myInteger);
+            int anotherInteger = 25;
+            Console.WriteLine(myInteger + anotherInteger);
+            
+            double myDouble = 7.1976;
+            Console.WriteLine(myDouble);
+
+            bool myBool = true;
+            myBool = false;
+            Console.WriteLine(myBool);
+
+            char myChar = '#';
+            char anotherChar = 'A';
+            Console.WriteLine(myChar);
+            Console.WriteLine(anotherChar);
+            Console.WriteLine(myChar+anotherChar); // adds the ASCII values together
+
+            string myString = "This is a string";
+            string anotherString = "and so is this";
+            Console.WriteLine(myString);
+            Console.WriteLine(anotherString);
+            Console.WriteLine(myString+anotherString);//concatenate
+            Console.WriteLine(myString+myChar); // Concatenates the string with the char
+
         }
+     
+
     }
 }
